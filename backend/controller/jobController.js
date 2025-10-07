dc2925ba23e363c0c4b6a91ad3a253906da710f7
Clyde <cdarizala@gbox.adnu.edu.ph>
2025-10-07 14:58:53 +0800

    job-04

--- backend/controller/jobController.js
+++ backend/controller/jobController.js
@@ -1,5 +1,4 @@
-import { createJobService } from "../services/jobService.js";
-
+import { createJobService, updateJobService, deleteJobService } from "../services/jobService.js";
 // Create Job
 export const createJob = async (req, res) => {
   try {
@@ -15,5 +14,33 @@ export const createJob = async (req, res) => {
     res.status(201).json(job);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+// Update Job
+export const updateJob = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const jobData = {
+      title: req.body.title,
+      description: req.body.description,
+      deadline: req.body.deadline,
+    };
+
+    const job = await updateJobService(jobId, jobData);
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete Job
+export const deleteJob = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await deleteJobService(jobId);
+    res.status(200).json({ message: "Job deleted successfully", job });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 };
